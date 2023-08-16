13087d309a1b440e1242cb82ff76e093368980c4
Javi <118106278+BoKatanKrize@users.noreply.github.com>
2023-08-16 03:52:49 +0200

    Update

--- src/models/register_model.py
+++ src/models/register_model.py
@@ -8,7 +8,7 @@ from utils.wandb import init_wandb_run, download_wandb_artifact, \
                         select_best_models_from_sweep, \
                         download_best_models, \
                         promote_model_to_registry
-from utils.io import load_data, save_pipe, save_model
+from utils.io import load_data, save_pipe
 from utils.aws_s3 import save_to_s3_bucket
 from .train import prepare_data
 
@@ -66,13 +66,8 @@ def main(params):
 
     logger.info(f'Saving best model locally...')
 
-    # Here PREFECT has a bug and will not save in tmp/ (works out of orchestration)
-    # ----------------------------------------------------------------------------
-    # info_pipe = save_pipe(best_model['model'],
-    #                       info_pipe, suffix=best_model['id'])
-
-    # Instead best model is only saved to S3 bucket
-    info_pipe = save_model(info_pipe, suffix=best_model['id'])
+    info_pipe = save_pipe(best_model['model'],
+                          info_pipe, suffix=best_model['id'])
 
     logger.info(f'Saving best model in S3 Bucket (LocalStack)...')
     save_to_s3_bucket(params["s3_bucket_name"],
--- src/utils/io.py
+++ src/utils/io.py
@@ -27,18 +27,8 @@ def save_pipe(pipe, info_pipe, suffix=None):
     pre = info_pipe["prefix_name"]
     pipe_name = f'{pre}.pkl' if suffix is None else f'{pre}_{suffix}.pkl'
     output_file_path = f'{info_pipe["path_local_out"]}/{pipe_name}'
-    with open(output_file_path, 'wb') as output_file:
+    with open('./', 'wb') as output_file:
         pickle.dump(pipe, output_file)
-    if info_pipe["fnames"] is None:
-        info_pipe["fnames"] = [pipe_name]
-    else:
-        info_pipe["fnames"].append(pipe_name)
-    return info_pipe
-
-
-def save_model(info_pipe, suffix=None):
-    pre = info_pipe["prefix_name"]
-    pipe_name = f'{pre}.pkl' if suffix is None else f'{pre}_{suffix}.pkl'
     if info_pipe["fnames"] is None:
         info_pipe["fnames"] = [pipe_name]
     else:
